95a96b71378849556c66334ac17d96fcd85f6cef
Samuel Khan <89487801+sambkhan@users.noreply.github.com>
2023-12-09 20:42:05 -0500

    Add two new metrics

--- src/urlparse_cmd/process_url.ts
+++ src/urlparse_cmd/process_url.ts
@@ -4,6 +4,7 @@ import logger from '../logger';
 import { GithubAPIService } from './metric_calc/git_API_call'
 import { cleanupTempDir, cloneRepoLocally } from './metric_calc/local_clone';
 import { MetricScores } from './metric_calc/pkg_metric';
+import { url } from 'inspector';
 
 class MetricScoreResults {
     //General purpose class  
@@ -16,6 +17,8 @@ class MetricScoreResults {
     bus_factor: number = 0;
     maintainer: number = 0;
     license: number = 0;
+    pinned_frac: number = 0;
+    reviewed_pull_frac: number = 0;
     clone_path: string = '';
 
     constructor (url: string) {
@@ -54,19 +57,22 @@ class MetricScoreResults {
     }
 
     calc_net_score() {
-        this.net_score = (this.license) * (this.bus_factor * 0.40 + 0.25 * (this.correctness + this.maintainer) + 0.1 * this.ramp_up)
+        this.net_score = (this.license) * (0.25*this.bus_factor + 0.17*(this.correctness + this.maintainer) + 0.11*this.ramp_up + 0.15*(this.pinned_frac + this.reviewed_pull_frac))
+    }
+
+    print_scores() {
+        //TAs advised to technically not do it like this but whatever its fine
+        console.log(`{"URL":"${this.url}", "NET_SCORE":${parseFloat(this.net_score.toFixed(5))}, "RAMP_UP_SCORE":${parseFloat(this.ramp_up.toFixed(5))}, "CORRECTNESS_SCORE":${parseFloat(this.correctness.toFixed(5))}, "BUS_FACTOR_SCORE":${parseFloat(this.bus_factor.toFixed(5))}, "RESPONSIVE_MAINTAINER_SCORE":${parseFloat(this.maintainer.toFixed(5))}, "LICENSE_SCORE":${parseFloat(this.license.toFixed(5))}}`) //Not sure if doing it like this is ok?
     }
 }
 
 
 
-export default async function get_metric_scores(filename: string) : Promise<any> {
+export default async function get_metric_scores(filename: string) {
 
     // if(filename.charAt(0) != "/") { //Check if the input is an actual filepath
     //     throw new Error("Invalid command given, command must be one of ./run (install | test | URL_FILE)")
     // }
-    let allScores = [];
-
 
     //Step 1: Open file
     try {
@@ -126,6 +132,8 @@ export default async function get_metric_scores(filename: string) : Promise<any>
                         url_metrics.license = scores.getLicense()
                         url_metrics.maintainer = await scores.getResponsiveness();
                         url_metrics.correctness = scores.getCorrectness();
+                        url_metrics.pinned_frac = scores.getPinnedDependenciesFraction();
+                        url_metrics.reviewed_pull_frac = scores.getCodeReviewFraction();
                         
                         //Once all 5 scores are calculated, update net score using our formula
                         //If any errors occur within the subscores, we just set them to 0
@@ -156,7 +164,6 @@ export default async function get_metric_scores(filename: string) : Promise<any>
 
             if(await url_metrics.init_api_caller(owner_name, repo_name)) { //Essentially the same as above minus a few steps
                 try {
-                    // @ts-ignore
                     url_metrics.clone_path = await cloneRepoLocally(url_metrics.repo_obj.clone_url, url_metrics.repo_obj.name)
                     logger.info(`Successfully cloned repo for ${url_list[i]} locally`)
                     logger.debug(`Repo clone located at ${url_metrics.clone_path}`)
@@ -168,6 +175,8 @@ export default async function get_metric_scores(filename: string) : Promise<any>
                     url_metrics.license = scores.getLicense();
                     url_metrics.maintainer = await scores.getResponsiveness();
                     url_metrics.correctness = scores.getCorrectness();
+                    url_metrics.pinned_frac = scores.getPinnedDependenciesFraction();
+                    url_metrics.reviewed_pull_frac = scores.getCodeReviewFraction();
                     url_metrics.calc_net_score()
                     logger.debug(`Finished calculating score for ${url_list[i]}`)
 
@@ -185,20 +194,9 @@ export default async function get_metric_scores(filename: string) : Promise<any>
             logger.error("Invalid link, link must be of the form https://www.npmjs.com/package/{name} or https://www.github.com/{repo}/{owner}")
         }
 
-        const score = {
-            "URL": url_metrics.url,
-            "NET_SCORE": parseFloat(url_metrics.net_score.toFixed(5)),
-            "RAMP_UP_SCORE": parseFloat(url_metrics.ramp_up.toFixed(5)),
-            "CORRECTNESS_SCORE": parseFloat(url_metrics.correctness.toFixed(5)),
-            "BUS_FACTOR_SCORE": parseFloat(url_metrics.bus_factor.toFixed(5)),
-            "RESPONSIVE_MAINTAINER_SCORE": parseFloat(url_metrics.maintainer.toFixed(5)),
-            "LICENSE_SCORE": parseFloat(url_metrics.license.toFixed(5))
-        };
-        allScores.push(score);
+        url_metrics.print_scores(); //Prints the NDJSON
 
     }
-
-    return allScores;
 
 }
 

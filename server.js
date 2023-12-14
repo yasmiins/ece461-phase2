09ba00924e5a8590f87edd2c5524b05c7e953281
dulaninw <89588053+dulaninw@users.noreply.github.com>
2023-12-14 00:12:03 -0500

    Update server.js

--- server.js
+++ server.js
@@ -370,7 +370,7 @@ app.put('/package/:id', async (req, res) => {
             return res.status(400).send({ message: "Invalid request data: Metadata and data are required" });
         }
 
-        const requiredMetadataFields = ["Name", "Version", "ID"];
+        const requiredMetadataFields = ["name", "version", "iD"];
         for (const field of requiredMetadataFields) {
             if (!metadata[field]) {
                 logger.warn(`Invalid request data: Missing ${field}`);
